6a4b03465c771d8d8e1c38f92b6466c0b41f7345
Luna R <snowluna@protonmail.com>
2022-08-08 08:39:07 -0400

    update from snapshot

--- src/components/About/__tests__/index.test.js
+++ src/components/About/__tests__/index.test.js
@@ -1,21 +1,19 @@
-import React from "react";
-import { render, cleanup } from "@testing-library/react";
-import "@testing-library/jest-dom/extend-expect";
-import About from "../";
+// __tests__/About.test.js
+import React from 'react';
+import { render, cleanup } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import About from '..';
 
 afterEach(cleanup);
 
-describe("About component", () => {
-   // Renders About test
-   //First Test
-   it("renders", () => {
-      render(<About />);
-   });
-
-   // Second Test
-   it("matches snapshot DOM node structure", () => {
-      // render
-      const { asFragment } = render(<About />);
-      expect(asFragment()).toMatchSnapshot();
-   })
+describe('About component renders', () => {
+  it('renders', () => {
+    render(<About />);
+  });
+  
+  it('matches snapshot DOM node structure', () => {
+    const { asFragment } = render(<About />);
+    
+    expect(asFragment()).toMatchSnapshot();
+  });
 })
